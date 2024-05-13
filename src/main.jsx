4735042a8f62b650c808d479c44fec2c1b387b6f
Armin Rueegger <armin.rueegger@ergon.ch>
2024-05-13 14:28:44 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,19 @@
+import React, {} from 'react'
+import App from './App.jsx'
+import './index.css'
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Main() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
