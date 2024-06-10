47ef3753d511f6f8fda438b1ded3f72aa6266ebf
Armin Rueegger <armin.rueegger@ergon.ch>
2024-06-10 13:45:39 +0200

    makes some minor changes

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export function Games({ points, setPoints }) {
     const handlePointChange = (event) => {
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {Link} from "react-router-dom";
 
 function Home() {
   const [count, setCount] = useState(0)
