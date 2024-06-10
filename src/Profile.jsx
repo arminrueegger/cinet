205c914c2ad9cb14a0ef2de2023bb1a142c2c254
sib <silvan.brors@icloud.com>
2024-06-10 16:36:28 +0200

    profile site

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,9 +1,15 @@
 import {Link} from "react-router-dom";
+import "./profile-style.css";
 
 export function Profile({name, setName, email, setEmail, phone, setPhone}) {
 
     return (
         <>
+            <img src="src/assets/RüeggerArmin.jpg" alt={"Profile Picture"}/>
+
+            <h1>{name}</h1>
+            <h1>{email}</h1>
+            <h1>{phone}</h1>
 
         </>
     )
